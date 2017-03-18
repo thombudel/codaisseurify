bfd157a471271e1c63d05768b44d9b5ba700fc21
thom budel <thombudel@gmail.com>
2017-03-18 12:13:19 +0100

    Created song model + song seeds

---
+++ app/models/song.rb
@@ -0,0 +1,5 @@
+class Song < ApplicationRecord
+  def self.order_by_name
+    order(:name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+#songs
+
+song1 = Song.create!({
+name: "Where's the Revolution",
+duration: 200,
+genre: "Electronic Pop"
+})
+song2 = Song.create!({
+name: "White Feather",
+duration: 180,
+genre: "Hard Rock"
+})
+song3 = Song.create!({
+name: "Do You Still Love Me?",
+duration: 240,
+genre: "Alt Country"
+})
