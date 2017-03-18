07284a1bc3fe859ca1cdfc111b7419f7083d5c2a
thom budel <thombudel@gmail.com>
2017-03-18 14:10:05 +0100

    Added song validations + error msg new song

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,9 @@ class Song < ApplicationRecord
   def self.order_by_name
     order(:name)
   end
+
+  validates :name, presence: true, uniqueness: true,length: {maximum: 30}
+  validates :duration, length: { maximum: 10 }, presence: true
+  validates :genre, length: { maximum: 30 }, presence: true
+
 end
